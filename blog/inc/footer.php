bf3deb345856a341f15e9e6dbf0e2ec243923d37
E11enses <69436814+E11enses@users.noreply.github.com>
2025-11-20 22:28:10 +0000

    footer changes

--- blog/inc/footer.php
+++ blog/inc/footer.php
@@ -10,8 +10,8 @@
   <div class="go-top"><a class="smoothscroll" title="Back to Top" href="#top"></a></div>
 </footer>
 </div>
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+<script src="/blog/js/jquery-3.2.1.min.js"></script>
+<script src="/blog/js/plugins.js"></script>
+<script src="/blog/js/main.js"></script>
 </body>
 </html>
