05d2634fffa41d4b7ed27f938cb9e24a63131f82
E11enses <69436814+E11enses@users.noreply.github.com>
2025-11-21 16:16:19 +0000

    Blog changs

--- blog/inc/header.php
+++ blog/inc/header.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Typerite</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/blog/css/base.css">
-  <link rel="stylesheet" href="/blog/css/vendor.css">
-  <link rel="stylesheet" href="/blog/css/main.css">
-  <script src="/blog/js/modernizr.js"></script>
-  <link rel="apple-touch-icon" sizes="180x180" href="/blog/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/blog/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/blog/favicon-16x16.png">
-  <link rel="manifest" href="/blog/site.webmanifest">
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="yandex-verification" content="049701e98722a755" />
+
+    <title>Блог МРАКЕТПЛЕЙСЫ</title>
+    <meta name="description"
+        content="Ищете фулфилмент? Мы поможем с хранением, упаковкой, отгрузкой и ведением магазина на маркетплейсах. Звоните +7 916 240 44 26">
+    <meta name="keywords"
+        content="фулфилмент, фулфилмент для маркетплейсов, склад, хранение, упаковка, доставка, ведение магазина, Wildberries, Ozon, Яндекс Маркет">
+
+    <!-- Open Graph -->
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="ru_RU" />
+    <meta property="og:title" content="Фулфилмент для маркетплейсов | МРАКЕТПЛЕЙСЫ" />
+    <meta property="og:description"
+        content="Хранение, упаковка, отгрузка и ведение магазинов на маркетплейсах. Опыт с Wildberries, Ozon, Яндекс.Маркет, Авито." />
+    <meta property="og:url" content="https://mraketplace.ru/" />
+    <meta property="og:site_name" content="МРАКЕТПЛЕЙСЫ" />
+    <meta property="og:image" content="https://mraketplace.ru/images/og-preview.jpg" />
+    <meta property="og:image:alt" content="Фулфилмент для маркетплейсов — МРАКЕТПЛЕЙСЫ" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+
+    <!-- Twitter (использует OG-данные, но может дополняться своими тегами) -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Фулфилмент для маркетплейсов | МРАКЕТПЛЕЙСЫ" />
+    <meta name="twitter:description" content="Услуги хранения, упаковки и продвижения для продавцов на маркетплейсах" />
+    <meta name="twitter:image" content="https://mraketplace.ru/images/og-preview.jpg" />
+
+    <meta name="author" content="МРАКЕТПЛЕЙСЫ">
+    <meta property="yandex" content="noretranslate">
+    <link rel="canonical" href="https://mraketplace.ru/" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
 </head>
 <body>
 <div id="preloader"><div id="loader" class="dots-fade"><div></div><div></div><div></div></div></div>
@@ -23,20 +53,20 @@
     <div class="header__top">
       <div class="header__logo">
         <a class="site-logo" href="index.php">
-          <img src="/blog/images/logo.svg" alt="Homepage">
+          МРАКЕТПЛЕЙСЫ
         </a>
       </div>
     </div>
     <nav class="header__nav-wrap">
       <ul class="header__nav">
-        <li class="current"><a href="index.php" title="">Home</a></li>
-        <li><a href="#" title="">Categories</a></li>
-        <li><a href="#" title="">Blog</a></li>
-        <li><a href="#" title="">About</a></li>
-        <li><a href="#" title="">Contact</a></li>
+        <li class="current"><a href="index.php" title="">Главная</a></li>
+        <li><a href="#" title="">Категории</a></li>
+        <li><a href="#" title="">Блог</a></li>
+        <li><a href="#" title="">О нас</a></li>
+        <li><a href="#" title="">Контакты</a></li>
       </ul>
       <ul class="header__social">
-        <li class="ss-facebook"><a href="https://facebook.com/"><span class="screen-reader-text">Facebook</span></a></li>
+        <li class="ss-vk"><a href="https://vk.com"><span class="screen-reader-text">VK</span></a></li>
       </ul>
     </nav>
     <a href="#0" class="header__menu-toggle"><span>Menu</span></a>
